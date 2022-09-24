2f9fac05cca9a3a0ded20d8adc9144c541fba8cd
lgangloff <loic.gangloff@gmail.com>
2022-09-24 22:49:44 +0200

    fix import

--- src/app/vaccination.service.ts
+++ src/app/vaccination.service.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Booking } from './booking';
 import { CENTERS } from './mock-vaccination-center';
 import { VaccinationCenter } from './vaccination-center';
 
